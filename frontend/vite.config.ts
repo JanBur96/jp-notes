441f370b079e4cf911392a0061b83268a5f74caa
Jan-Philipp <jan.burghardt1706@gmail.com>
2026-03-30 19:54:06 +0200

    initialize frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  proxy: {
+    "/api": {
+      target: "http://localhost:4000",
+    },
+  },
+});
